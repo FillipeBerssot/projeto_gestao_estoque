063961b6e427fc07728c8da843fae9af79355a0a
fillipeberssot95@hotmail.com <fillipeberssot95@hotmail.com>
2025-06-03 17:10:44 -0300

    Implementa funcionalidades de login e logout de usuário

--- minha_app/auth/routes.py
+++ minha_app/auth/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, url_for, flash, request
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth_bp
 from ..models import User
 from .. import db
@@ -44,3 +44,32 @@ def register():
             flash(error, 'danger')
 
     return render_template('auth/register.html')
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.check_password(password):
+            login_user(user)
+            flash('Login bem-sucedido!', 'success')
+
+            next_page = request.args.get('next')
+            return redirect(next_page or url_for('main.index'))
+        else:
+            flash('Nome de usuário ou senha inválidos. Tente novamente.', 'danger')
+
+    return render_template('auth/login.html')
+
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Voçê foi desconectado com sucesso!', 'info')
+    return redirect(url_for('main.index'))
